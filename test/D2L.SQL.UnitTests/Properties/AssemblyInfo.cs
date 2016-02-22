7cdb1e4295351ca2477658de8c873b4425add0ac
Mark Tse <mark.tse@d2l.com>
2016-02-22 12:36:31 -0500

    Adding values for nuspec.

--- test/D2L.SQL.UnitTests/Properties/AssemblyInfo.cs
+++ test/D2L.SQL.UnitTests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("D2L.SQL.UnitTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# libraries for handling SQL.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("D2L")]
 [assembly: AssemblyProduct("D2L.SQL.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © D2L 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
